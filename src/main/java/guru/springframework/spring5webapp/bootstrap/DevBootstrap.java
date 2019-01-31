ad60a19a1951989f28385a5af711b197ab1f9209
nik1168 <ngeisser32@gmail.com>
2019-02-01 00:49:52 +0100

    Creating Publisher entity along with repository and adding it to dev bootstrap class

--- src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/DevBootstrap.java
@@ -3,9 +3,11 @@ package guru.springframework.spring5webapp.bootstrap;
 import guru.springframework.spring5webapp.model.Author;
 import guru.springframework.spring5webapp.model.Book;
 
+import guru.springframework.spring5webapp.model.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
 
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
@@ -14,10 +16,12 @@ import org.springframework.stereotype.Component;
 public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> {
     private AuthorRepository authorRepository;
     private BookRepository bookRepository;
+    private PublisherRepository publisherRepository;
 
-    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public DevBootstrap(AuthorRepository authorRepository, BookRepository bookRepository, PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
@@ -29,18 +33,24 @@ public class DevBootstrap implements ApplicationListener<ContextRefreshedEvent> 
     private void initData(){
         //Jose Madero
         Author jose = new Author("Jose", "Madero");
-        Book  pbpm = new Book("Pensandolo bien Pense Mal", "1234", "Movic");
+        Publisher movic = new Publisher("Movic","Monterey Nueva Leon");
+        publisherRepository.save(movic);
+        Book  pbpm = new Book("Pensandolo bien Pense Mal", "1234", movic);
         jose.getBooks().add(pbpm);
         pbpm.getAuthors().add(jose);
         authorRepository.save(jose);
         bookRepository.save(pbpm);
 
+
         //Sthepen King
         Author sthepen = new Author("Sthepen", "King");
-        Book  drDream = new Book("Dr. Sueno", "12345", "ST");
+        Publisher st = new Publisher("ST","Chicago");
+        publisherRepository.save(st);
+        Book  drDream = new Book("Dr. Sueno", "12345", st);
         sthepen.getBooks().add(drDream);
         drDream.getAuthors().add(sthepen);
         authorRepository.save(sthepen);
         bookRepository.save(drDream);
+
     }
 }
